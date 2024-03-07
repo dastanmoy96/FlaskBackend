5753cd071240b060b8e87c4cb6a6b44fe1871d46
The_Silent_Protagonist <94416945+dastanmoy96@users.noreply.github.com>
2024-03-08 03:40:29 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+import pandas as pd
+
+model = pickle.load(open('E:/MiniProject2024/MiniProject2024Backend/Models/model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    latitude = pd.to_numeric(request.form.get('latitude'))
+    longitude = pd.to_numeric(request.form.get('longitude'))
+    wind = pd.to_numeric(request.form.get('wind'))
+    wind_degree = pd.to_numeric(request.form.get('wind_degree'))
+    pressure = pd.to_numeric(request.form.get("pressure"))
+    precipitation = pd.to_numeric(request.form.get("precipitation"))
+    humidity = pd.to_numeric(request.form.get("humidity"))
+    cloud = pd.to_numeric(request.form.get("cloud"))
+    temperature = "temperature"
+    input_query = np.array([[latitude, longitude, wind, wind_degree, pressure, precipitation, humidity, cloud]])
+    result = model.predict(input_query)[0]
+    return jsonify({'temperature' : str(result)})
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
